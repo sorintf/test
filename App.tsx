5a4f151d563c38d838712874d2a09f64326350f7
sorintf <sorin.dan@twoandfrom.com>
2023-08-31 17:46:26 +0300

    just starting

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+const App = () => {
+  return (
+    <View>
+      <View>
+        <Text>
+          Hello 1
+        </Text>
+      </View>
+      <View>
+        <Text>
+          Hello 2
+        </Text>
+      </View>
+    </View>
+  )
+}
+
+
+export default App;
